c5fd1c0b98152f57f7a88decc7f5bf674145cc05
Ulyana-ko <72019605+Ulyana-ko@users.noreply.github.com>
2021-03-02 11:59:03 +0300

    Add files via upload

---
+++ AlgGrehema/cmake-build-debug/CMakeFiles/AlgGrehema.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "AlgGrehema.exe"
+  "AlgGrehema.exe.manifest"
+  "AlgGrehema.pdb"
+  "CMakeFiles/AlgGrehema.dir/main.c.obj"
+  "libAlgGrehema.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/AlgGrehema.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
